ed7b57ef53ab541694d8ae499b5bc6966bd3d458
Serial-Comma <alliwantisana1@gmail.com>
2023-04-23 21:03:49 +0800

    wasd

--- app.py
+++ app.py
@@ -1,58 +1,77 @@
 import os
+from flask import Flask, render_template, send_from_directory, url_for, g
+from werkzeug.utils import secure_filename
 import sqlite3
-from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = 'pdfs'
-app.config['DATABASE'] = 'pdfs.db'
 
-# Define the database schema and create a connection
-def create_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    c = conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS pdf_files
-                 (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                  filename TEXT NOT NULL,
-                  path TEXT NOT NULL)''')
-    conn.commit()
-    conn.close()
+# Define the directory where PDF files are stored
+PDF_DIR = 'pdfs'
 
-# Add PDF files to the database
-def add_pdf_files_to_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    c = conn.cursor()
-    for filename in os.listdir(app.config['UPLOAD_FOLDER']):
-        if filename.endswith('.pdf'):
-            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            secure_filename = filename.replace(' ', '_')
-            c.execute("INSERT INTO pdf_files (filename, path) VALUES (?, ?)",
-                      (secure_filename, filepath))
-    conn.commit()
-    conn.close()
+# Define the name of the SQLite database file
+DB_FILENAME = 'pdfs.db'
 
-# Retrieve PDF files from the database
-def get_pdf_files_from_db():
-    conn = sqlite3.connect(app.config['DATABASE'])
-    c = conn.cursor()
-    c.execute("SELECT filename, path FROM pdf_files")
-    pdf_files = c.fetchall()
-    conn.close()
-    return pdf_files
+# Define the full path to the database file
+DB_PATH = os.path.join(app.root_path, DB_FILENAME)
+
+# Define the database schema
+SCHEMA = '''
+CREATE TABLE IF NOT EXISTS pdf_files (
+    id INTEGER PRIMARY KEY,
+    filename TEXT UNIQUE,
+    filepath TEXT
+);
+'''
+
+# Create a connection to the database
+conn = sqlite3.connect(DB_PATH)
+
+# Create the pdf_files table if it doesn't already exist
+with conn:
+    conn.execute(SCHEMA)
+
+# Add all PDF files to the database
+for filename in os.listdir(PDF_DIR):
+    if filename.endswith('.pdf'):
+        filepath = os.path.join(PDF_DIR, filename)
+        securefilename = secure_filename(filename)
+        with conn:
+            conn.execute('INSERT OR IGNORE INTO pdf_files (filename, filepath) VALUES (?, ?);', (securefilename, filepath))
+
+@app.teardown_appcontext
+def close_db(error):
+    if 'db' in g:
+        g.db.close()
+
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect('pdfs.db')
+    return g.db
+
+# Define the route for serving PDF files
+@app.route('/pdfs/<path:filename>')
+def serve_pdf(filename):
+    return send_from_directory(PDF_DIR, filename)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Define the route for the PDF list page
+
 @app.route('/pdfs')
 def pdfs():
-    pdf_files = get_pdf_files_from_db()
+    conn = get_db()
+    pdf_files = conn.execute('SELECT filename, filepath FROM pdf_files').fetchall()
     return render_template('pdfs.html', pdf_files=pdf_files)
 
-@app.route('/pdfs/<filename>')
-def serve_pdf(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
 
 if __name__ == '__main__':
-    create_db()
-    add_pdf_files_to_db()
-    app.run(debug=True)
+    app.run()
